c82ae96e4ca27668a1688c4c8add8a7e64eaa350
Mary Hudner <Mhudner@yahoo.com>
2024-02-23 15:35:18 -0700

    add .gitignore

---
+++ src/com/skilldistillery/foodtruck/FoodTruckApp.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.foodtruck;
+
+public class FoodTruckApp {
+	private maxTrucks = 5;
+	private FoodTruck[] fleet = new FoodTruck[maxTrucks];
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FoodTruckApp fta = new FoodTruckApp();
+		fta.run();
+	}
+	public void run() {
+		
+	}
+}
